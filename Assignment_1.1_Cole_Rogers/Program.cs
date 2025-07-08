76104f5f9b6ce17dc97883ae3364502b693aed64
15127 <15127@DESKTOP-0V74CCT>
2025-07-08 10:28:00 -0500

    Add project files.

---
+++ Assignment_1.1_Cole_Rogers/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Cole Rogers";
+int age = 22;
+string address = "Texas, Austin";
+
+Console.WriteLine(name + " is " + age + " and lives in " + address);
